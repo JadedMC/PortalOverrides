ff2346d3886bdcf7628c9a430e0528237e2debc0
colby <colbykrawczyk10@gmail.com>
2023-04-21 21:40:18 -0400

    Adds a base setup for the config file

--- src/main/java/net/jadedmc/portaloverrides/PortalOverrides.java
+++ src/main/java/net/jadedmc/portaloverrides/PortalOverrides.java
@@ -2,16 +2,27 @@ package net.jadedmc.portaloverrides;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+/**
+ * This class is the main class of the plugin.
+ * It links all parts together and registers them with the server.
+ */
 public final class PortalOverrides extends JavaPlugin {
+    private SettingsManager settingsManager;
 
+    /**
+     * Runs when the server is started.
+     */
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        settingsManager = new SettingsManager(this);
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    /**
+     * Gets the current settings manager instance.
+     * @return Settings Manager.
+     */
+    public SettingsManager getSettingsManager() {
+        return settingsManager;
     }
 }
